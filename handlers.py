87862559e6dcec9a787599eba139bec9a1b4e76a
Daniel Gomez Jurado <danielgomjur@gmail.com>
2013-01-30 20:25:23 +0100

    Implemented render function

--- handlers.py
+++ handlers.py
@@ -1,6 +1,10 @@
 
 import webapp2
 
+import os
 
 class Base(webapp2.RequestHandler):
-  pass
+  def render(self, name, *args, **kwargs):
+    template = jinja_environment.get_template(os.path.join("templates", 
+        ('%s.html' % name) ))
+    self.response.out.write(template.render(*args, **kwargs))
